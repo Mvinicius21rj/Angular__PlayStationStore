2cc5e457bc151d829aa7c84718ea6053d1b4056e
MarcosVinicius <mviniciusrj21@gmail.com>
2024-08-08 14:58:59 -0300

    Funcional

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CardComponent, CardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
